d76b77dbadfbd5efc0a5b60aa096a688081399fe
Matthew Marshall <matthew.marshall@manseds.co.uk>
2017-07-14 23:56:50 +0100

    Implement proximity sensor script.

--- proximity/init.py
+++ proximity/init.py
@@ -7,37 +7,47 @@
 # This file initiailises the proximity sensors.
 # The proximity sensors are digital and of two classes: 5cm trigger and 10cm trigger distances.
 
-import RPi.GPIO as GPIO
+from datetime import datetime
 import os
+import sys
+
+import RPi.GPIO as GPIO
+
+sys.path.insert(0, "../Robotics/Motor_brake.py")
+
+from Motor_brake import motor_brake
 
 # Edit these as necessary for changing pin locations.
-near_pins = [0, 0, 0]
-far_pins = [0, 0, 0]
+NEAR_PINS = [0, 0, 0]
+FAR_PINS = [0, 0, 0]
 
 num = 0
 
-if os.path.isfile("/var/www/html/proximity.dat"):
-    with open("/var/www/html/proximity.dat", "r") as handle:
-        lines = handle.readlines()
-        num = int(lines[-1].split()[0])
-
-
-# GPIO setup
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(near_pins, GPIO.IN)
-GPIO.setup(far_pins, GPIO.IN)
-
-
 def handle_5cm():
-    
-
+    global num
+    motor_brake()
+    with open("/var/www/html/proximity.dat", "a+") as handle:
+        handle.write("{} {} {}", num, 1, datetime.now())
 
 def handle_10cm():
-    
+    global num
+    with open("/var/www/html/proximity.dat", "a+") as handle:
+        handle.write("{} {} {}", num, 2, datetime.now())
 
+def init():
+    global num
 
-# Main
+    if os.path.isfile("/var/www/html/proximity.dat"):
+        with open("/var/www/html/proximity.dat", "r") as handle:
+            lines = handle.readlines()
+            num = int(lines[-1].split()[0])
+
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(NEAR_PINS, GPIO.IN)
+    GPIO.setup(FAR_PINS,  GPIO.IN)
+
+    GPIO.add_event_detect(NEAR_PINS, GPIO.FALLING, callback=handle_5cm)
+    GPIO.add_event_detect(FAR_PINS, GPIO.FALLING, callback=handle_10cm)
+
 if __name__ == "__main__":
-    GPIO.add_event_detect(near_pins, GPIO.FALLING, callback=stop)
-    GPIO.add_event_detect(far_pins, GPIO.FALLING, callback=warn)
-    
+    init()
