bfc8fbe0d3108720bbb3b1e3d01c50f85fc8e479
Matthew Marshall <matthew.marshall@manseds.co.uk>
2017-07-14 10:12:24 +0100

    Check if sensor as already ran, and if so continue counting from last index value.

--- run.py
+++ run.py
@@ -1,9 +1,16 @@
 import init
 import read
+import os
 
 init.init()
 
 num = 0
+
+if os.path.isfile("/var/www/html/sensors.dat"):
+    with open("/var/www/html/sensors.dat", "r") as handle:
+        lines  = handle.readlines()
+        num = int(lines[-1].split()[0])
+
 while True:
     read.read(num)
     num += 1
