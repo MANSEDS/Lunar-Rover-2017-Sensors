e5ff340930b3d9f0dcdeb749c2f3b9780f97d478
Matthew Marshall <matthew.marshall@manseds.co.uk>
2017-07-18 19:05:04 +0100

    Choose a smarter capture rate.

--- thermal/run.py
+++ thermal/run.py
@@ -15,7 +15,10 @@ def run():
             continue
         convert(i % 2)
         i += 1
-        time.sleep(0.1)
+        # Thermal sensor can only be captured from ~8 times a second.
+        # Take a safe value such as 5 per second to ensure the sensor
+        # does not "burnout" and not respond for a time.
+        time.sleep(0.2)
 
 if __name__ == "__main__":
     run()
